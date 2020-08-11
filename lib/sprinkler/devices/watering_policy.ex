cbc0adda4ca70fb2d4d8f278661b4aa55ccda710
Juan Manuel Ferreira <juanmferreira93@gmail.com>
2020-08-11 11:15:23 -0300

    refactor methods

--- lib/sprinkler/devices/watering_policy.ex
+++ lib/sprinkler/devices/watering_policy.ex
@@ -1,68 +1,84 @@
 defmodule Sprinkler.Devices.WateringPolicy do
-
   def should_irrigate(moisture_input, temperature_input, humidity_input) do
     unless soil_moisture_wet?(moisture_input) do
+      when_soil_moisture_not_wet(moisture_input, temperature_input, humidity_input)
+    end
+  end
 
-      if soil_moisture_medium?(moisture_input) do
-        if ambience_temperature_high?(temperature_input) do
-          irrigate('medium')
-        else
-          unless humidity_high?(humidity_input) do
-            irrigate('low')
-          end
-        end
-      else
-        if humidity_high?(humidity_input) do
-          if ambience_temperature_high?(temperature_input) do
-            irrigate('high')
-          else
-            irrigate('medium')
-          end
-        else
-          irrigate('high')
-        end
+  defp when_soil_moisture_not_wet(moisture_input, temperature_input, humidity_input) do
+    if soil_moisture_medium?(moisture_input) do
+      when_soil_moisture_medium(temperature_input, humidity_input)
+    else
+      when_soil_moisture_not_medium(temperature_input, humidity_input)
+    end
+  end
+
+  defp when_soil_moisture_medium(temperature_input, humidity_input) do
+    if ambience_temperature_high?(temperature_input) do
+      irrigate('medium')
+    else
+      unless humidity_high?(humidity_input) do
+        irrigate('low')
       end
     end
   end
 
-  def soil_moisture_wet?(moisture_input) do
-    if moisture_input > 70 do # TODO: check this values, 70%
+  defp when_soil_moisture_not_medium(temperature_input, humidity_input) do
+    if humidity_high?(humidity_input) do
+      if ambience_temperature_high?(temperature_input) do
+        irrigate('high')
+      else
+        irrigate('medium')
+      end
+    else
+      irrigate('high')
+    end
+  end
+
+  defp soil_moisture_wet?(moisture_input) do
+    # TODO: check this values, 70%
+    if moisture_input > 70 do
       true
     else
       false
     end
   end
 
-  def soil_moisture_medium?(moisture_input) do
-    if moisture_input > 50 do  #TODO: check this values, medium > 50%
+  defp soil_moisture_medium?(moisture_input) do
+    # TODO: check this values, medium > 50%
+    if moisture_input > 50 do
       true
     else
       false
     end
   end
 
-  def ambience_temperature_high?(temperature_input) do
-    if temperature_input > 25 do #TODO: check this values, high > 25 grades
+  defp ambience_temperature_high?(temperature_input) do
+    # TODO: check this values, high > 25 grades
+    if temperature_input > 25 do
       true
     else
       false
     end
   end
 
-  def humidity_high?(humidity_input) do
-    if humidity_input > 50 do  #TODO: check this values, high > 50%
+  defp humidity_high?(humidity_input) do
+    # TODO: check this values, high > 50%
+    if humidity_input > 50 do
       true
     else
       false
     end
   end
 
-  def irrigate(irrigate_input) do
+  defp irrigate(irrigate_input) do
     cond do
       irrigate_input == 'high' ->
         IO.puts("Irrigating high amount of water")
+
       irrigate_input == 'medium' ->
         IO.puts("Irrigating medium amount of water")
+
       true ->
         IO.puts("Irrigating low amount of water")
     end
